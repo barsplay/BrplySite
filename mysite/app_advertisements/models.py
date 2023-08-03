38debe34d537bb00f541acbaa9756c86d8804180
Brply <arsenijkot99@mail.ru>
2023-08-03 17:34:27 +0300

    lesson 8

--- mysite/app_advertisements/models.py
+++ mysite/app_advertisements/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
-class Advertisements(models.Model):
+class Advertisement(models.Model):
     title = models.CharField("Заголовок", max_length=60)
     description = models.TextField("Описание")
     price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
@@ -8,6 +10,15 @@ class Advertisements(models.Model):
     updated_at = models.DateTimeField("Дата обновления", auto_now=True)
     created_at = models.DateTimeField("Дата публикации", auto_now_add=True)
 
+    @admin.display(description="Дата публикации")
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M")
+            return format_html(
+                "<span style='color: green; font-weight: bold'>Сегодня в {}</span>", created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y - %H:%M ")
     class Meta:
         db_table = "advertisements"
 
