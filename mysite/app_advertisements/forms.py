f60edaece78e4f417b3984e9eb4a2ccad0df66fe
Brply <arsenijkot99@mail.ru>
2023-08-14 16:35:46 +0300

    lesson 10

---
+++ mysite/app_advertisements/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class AdvertisementForm(forms.Form):
+    title = forms.CharField(max_length=60)
+    description = forms.CharField(widget=forms.Textarea)
+    price = forms.DecimalField()
+    is_auction = forms.BooleanField(required=False)
+    image = forms.ImageField()
--- mysite/app_advertisements/views.py
+++ mysite/app_advertisements/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Advertisement
+from .forms import AdvertisementForm
+
+from django.core.handlers.wsgi import WSGIRequest
+from django.urls import reverse
 
 def index(request):
     advertisement = Advertisement.objects.all()
@@ -12,8 +16,25 @@ def index(request):
 def top_sellers(request):
     return render(request, "top-sellers.html")
 
-def advertisement_post(request):
-    return render(request, "advertisement-post.html")
+
+def advertisement_post(request:WSGIRequest):
+    
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            adv = Advertisement(**form.cleaned_data)
+            adv.user = request.user
+            adv.save()
+
+            return redirect(
+                reverse("index")
+            )
+    else:
+        form = AdvertisementForm()
+    context = {
+        'form':form
+    }
+    return render(request, "advertisement-post.html", context)
 
 def login(request):
     return render(request, "login.html")
